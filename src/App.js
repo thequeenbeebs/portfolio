0e1df002aa2f99e0c55b3cd163eb9d8a3d277c35
Blaire Baker <bakerblaire@gmail.com>
2021-06-07 18:30:08 -0600

    react router link

--- src/App.js
+++ src/App.js
@@ -11,7 +11,23 @@ import MainContainer from './components/MainContainer'
 export default function App() {
   return (
     <Router>
-
+      <nav>
+        <button>
+          <Link to="/">Home</Link>
+        </button>
+        <button>
+          <Link to="/about">About</Link>
+        </button>
+        <button>
+          <Link to="/resume">Resume</Link>
+        </button>
+        <button>
+          <Link to="/portfolio">Portfolio</Link>
+        </button>
+        <button>
+          <Link to="/connect">Connect</Link>
+        </button>
+      </nav>
     </Router>
   );
 }
