fbe5ea20dc77a4cd3fe6f246da3a294b89ef21c7
Blaire Baker <bakerblaire@gmail.com>
2021-07-29 15:35:07 -0600

    center align connect page and h2 elements

--- src/components/Connect.js
+++ src/components/Connect.js
@@ -2,7 +2,7 @@ export default function Connect() {
     return (
         <main>
             <h2 className="orange-background">Connect with Me</h2>
-            <div>
+            <div class="center-text">
                 <p>bakerblaire@gmail.com</p>
                 <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/blaire-baker/">LinkedIn</a> <br/>
                 <a target="_blank" rel="noreferrer" href="https://github.com/thequeenbeebs">GitHub</a><br/>
